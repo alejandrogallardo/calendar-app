fc8a8dfeb4bdaff439c9b088796bf69788be506f
ridesnstuffgt@gmail.com <alejandrogallardo>
2021-05-11 22:51:01 -0600

    Se agregaron eventos para manejar el calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import 'moment/locale/es';
 
 import { Navbar } from '../ui/Navbar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages';
+import { CalendarEvent } from './CalendarEvent';
 
+moment.locale('es');
 const localizer = momentLocalizer(moment);
 
 const events = [{
-    title: 'Cumple',
+    title: 'Cumple del perro',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Empezar proyecto Banrural',
+    user: {
+        _id: '123',
+        name: 'Alejandro'
+    }
 }]
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    }
+    
+    const onSelectEvent = (e) => {
+        console.log(e);
+    }
+    
+    const onViewChange = (e) => {
+        // console.log(e);
+        setLastView(e);
+        localStorage.setItem('lastView', e)
+    }
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        // console.log(event, start, end, isSelected);
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <div className="calendar-screen">
             <Navbar />
             
             <Calendar
-                localizer={localizer}
+                localizer={ localizer }
                 events={ events }
                 startAccessor="start"
                 endAccessor="end"
-                
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
             />
             {/* style={{ height: 500 }} */}
         </div>
